93bba45e3670b9aa21e0f0bfca40880b19ac00f7
Arken <Arken.Sarsenov@outlook.com>
2023-11-07 23:20:49 +0500

    Initial Commit

---
+++ START-HW14-Arken/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  START-HW14-Arken
+//
+//  Created by Arken Sarsenov on 07.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
